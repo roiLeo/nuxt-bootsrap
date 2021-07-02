2a48fd2467b52e38b35ad253d3707f021b42eb75
roiLeo <medina.leo42@gmail.com>
2021-07-02 10:13:26 +0200

    :wrench: init bootstrap 5 & nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+export default {
+	target: 'static',
+
+	head: {
+		title: 'nuxt-bootstrap',
+		htmlAttrs: {
+			lang: 'en'
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+			{ name: 'format-detection', content: 'telephone=no' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		]
+	},
+
+	css: [
+		'~/assets/scss/main.scss'
+	],
+
+	plugins: [
+		{ src: '~/plugins/bootstrap.js', mode: 'client' }
+	],
+
+	components: true,
+
+	buildModules: [
+	],
+
+	modules: [
+	],
+
+	build: {
+	}
+}
